7067b35e7e52ad77b9979f853e9ecef579f1fdd2
Jose David Quintero <quinteromda@gmail.com>
2021-06-29 14:58:09 -0400

    Estructura Basica del Proyecto, ademas de la maquetacion lista en el index.html

---
+++ js/preguntas.js
@@ -0,0 +1,56 @@
+const myQuestions = [
+    {
+        "pregunta": "¿En qué año el hombre pisó la Luna por primera vez?",
+        "respuestas": {
+            "a" : "1973",
+            "b" : "1969",
+            "c" : "1965",
+        },
+        "respuestaC": "1969"
+    },
+    {
+        "pregunta": "¿Cuánto duró la Guerra de los Cien Años?",
+        "respuestas": {
+            "a" : "100 años",
+            "b" : "100 años",
+            "c" : "116 años",
+        },
+        "respuestaC": "100 años"
+    },
+    {
+        "pregunta": "¿En qué año se creó la Organización de las Naciones Unidas?",
+        "respuestas": {
+            "a" : "1942",
+            "b" : "1945",
+            "c" : "1950",
+        },
+        "respuestaC": "1945"
+    },
+    {
+        "pregunta": "¿Cuál es la lengua más hablada del mundo?",
+        "respuestas": {
+            "a" : "Chino Mandarin",
+            "b" : "Ingles",
+            "c" : "Ruso",
+        },
+        "respuestaC": "Chino Mandarin"
+    },
+    {
+        "pregunta": "¿Dónde podemos encontrar la Casa Rosada?",
+        "respuestas": {
+            "a" : "Estados Unidos",
+            "b" : "España",
+            "c" : "Argentina",
+        },
+        "respuestaC": "Argentina"
+    },
+    {
+        "pregunta": "¿Cuál es el país de mayor tamaño del mundo?",
+        "respuestas": {
+            "a" : "China",
+            "b" : "Rusia",
+            "c" : "Canada",
+        },
+        "respuestaC": "Rusia"
+    },
+]
