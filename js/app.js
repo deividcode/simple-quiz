f7954e8693142b8171f5ab0bbbba7dc77814561a
Jose David Quintero <quinteromda@gmail.com>
2021-07-12 18:32:26 -0400

    eliminando carpeta sass

---
+++ js/app.js
@@ -0,0 +1,19 @@
+(function (){
+    // Variables
+    const respuestaInput = document.querySelectorAll('.respuesta__input');
+    
+    
+    // // Eventos
+    // window.addEventListener('load', () =>{        
+    //     respuestaInput.addEventListener('click', inputEfectos);
+    // });
+
+    // function inputEfectos(e){
+    //     respuestaInput.forEach(element => {
+            
+    //     });
+    //     console.log('hola');
+    // }
+
+
+})();
